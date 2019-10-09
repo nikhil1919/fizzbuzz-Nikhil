a1b2f7bf8fef1287a42490e5b0b0a9bf1c2386e8
nikhil1919 <Nikhil.Pahilajani@dal.ca>
2019-10-09 09:59:14 -0700

    final code of this task along with the comments

--- fizzbuzz-Nikhil/task03.py
+++ fizzbuzz-Nikhil/task03.py
@@ -1,9 +1,9 @@
 def multiples(num):
 	for num  in range(1,100):
-		if num % 3 == 0:
-			print("fizz")
-		elif num % 5 == 0:
-			print("buzz")
-		elif num % 15 == 0:
-			print("fizzbuzz")
+		if num % 3 == 0:  #number multiple of 3
+			print("Fizz")
+		elif num % 5 == 0:  #number multiple of 5
+			print("Buzz")
+		elif num % 15 == 0:  #number multiple of 3 and 5
+			print("FizzBuzz")
 multiples(75)
